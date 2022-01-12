6b144ab9065e2157659eb57984dea18bb9698ebc
robertodessi <roberto.dessi11@gmail.com>
2022-01-12 03:11:18 -0800

    fix padding in interaction

--- egg/zoo/referential_language/callbacks.py
+++ egg/zoo/referential_language/callbacks.py
@@ -3,27 +3,12 @@
 # This source code is licensed under the MIT license found in the
 # LICENSE file in the root directory of this source tree.
 
-import json
-
+import torch
 import wandb
 
+import torch.nn.functional as F
 from egg.core import Callback, ConsoleLogger, Interaction
 from egg.core.early_stopping import EarlyStopper
-
-
-class BestStatsTracker(Callback):
-    def __init__(self):
-        self.best = {"acc": -float("inf"), "loss": float("inf"), "epoch": -1}
-
-    def on_validation_end(self, loss, logs: Interaction, epoch: int):
-        if logs.aux["acc"].mean().item() > self.best["acc"]:
-            self.best["acc"] = logs.aux["acc"].mean().item()
-            self.best["loss"] = loss
-            self.best["epoch"] = epoch
-
-    def on_train_end(self):
-        best_stats = dict(mode="best_stats", **self.best)
-        print(json.dumps(best_stats), flush=True)
 
 
 class WandbLogger(Callback):
@@ -44,25 +29,62 @@ class WandbLogger(Callback):
 
     def on_validation_end(self, loss: float, logs: Interaction, epoch: int):
         acc = logs.aux["acc"].mean().item()
-        values = {"test_loss": loss, "test_acc": acc, "epoch": epoch}
+        values = {"val_loss": loss, "val_acc": acc, "epoch": epoch}
         self.log(values)
 
 
 class EarlyStopperAcc(EarlyStopper):
     def should_stop(self) -> bool:
-        if len(self.validation_stats) > 2:
+        if len(self.validation_stats) > 1:
             past_acc = self.validation_stats[-2][1].aux["acc"].mean().item()
             last = self.validation_stats[-1][1].aux["acc"].mean().item()
-            if (last - past_acc.item()) * 100 > 0.001:
+            if (last - past_acc) * 100 > 0.001:
                 return False
+            print("| EARLY STOPPING")
             return True
+
+        return False
+
+
+class InteractionPadder(Callback):
+    def __init__(self, max_objs):
+        self.max_objs = max_objs
+
+    def on_batch_end(
+        self, logs: Interaction, loss: float, batch_id: int, is_training: bool = True
+    ):
+        pad_len = self.max_objs - logs.aux["acc"].shape[0]
+        logs.aux["baseline"] = torch.Tensor([1 / logs.aux["acc"].shape[0]])
+        if not is_training:
+            for k, v in logs.aux_input.items():
+                if k == "attn_weights":
+                    logs.aux_input[k] = F.pad(v, (0, pad_len, 0, pad_len))
+
+                else:
+                    logs.aux_input[k] = F.pad(v, (0, 0, 0, pad_len))
+
+            logs.receiver_input = F.pad(logs.receiver_input, (0, 0, 0, pad_len))
+            logs.labels = F.pad(logs.labels, (0, pad_len), value=-1)
+            logs.message = F.pad(logs.message, (0, 0, 0, pad_len), value=-1)
+            logs.receiver_output = F.pad(
+                logs.receiver_output, (0, pad_len, 0, pad_len), value=-1
+            )
+            logs.aux_input["single_accs"] = F.pad(
+                logs.aux["acc"], (0, pad_len), value=-1
+            )
+            logs.aux_input["mask"] = torch.ones(self.max_objs)
+            if pad_len > 0:
+                logs.aux_input["mask"][-pad_len:] = 0
+
+        acc = logs.aux["acc"].mean().item()
+        logs.aux["acc"] = torch.Tensor([acc] * self.max_objs)
 
 
 def get_callbacks(opts):
     callbacks = [
-        BestStatsTracker(),
         ConsoleLogger(as_json=True, print_train_loss=True),
         EarlyStopperAcc(),
+        InteractionPadder(opts.max_objects),
     ]
     if opts.wandb:
         return callbacks + [WandbLogger()]
