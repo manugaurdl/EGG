135732c38df371b1f036068f96d61cbc3df3615e
robertodessi <roberto.dessi11@gmail.com>
2022-02-07 06:54:38 -0800

    rename utils and add sweeps

--- egg/zoo/referential_language/utils/opts.py
+++ egg/zoo/referential_language/utils/opts.py
@@ -28,30 +28,24 @@ def get_data_opts(parser):
         default=20,
         help="Max number of bboxes to extract from an image",
     )
-    group.add_argument("--image_size", type=int, default=64, help="Image size")
+    group.add_argument("--image_size", type=int, default=32, help="Image size")
     group.add_argument("--random_distractors", default=False, action="store_true")
 
 
 def get_vision_module_opts(parser):
     group = parser.add_argument_group("vision module options")
     group.add_argument(
-        "--vision_model_name",
+        "--vision_model",
         type=str,
-        default="resnet101",
-        choices=["resnet50", "resnet101", "resnet152"],
+        default="resnet34",
+        choices=["resnet18", "resnet34", "resnet50", "resnet101"],
         help="Model name for the encoder",
     )
     group.add_argument(
         "--pretrain_vision",
         default=False,
         action="store_true",
-        help="If set, pretrained vision modules will be used",
-    )
-    group.add_argument(
-        "--shared_vision",
-        default=False,
-        action="store_true",
-        help="If set, the vision module will be shared by sender and reciver",
+        help="If set, pretrained vision models will be used",
     )
 
 
@@ -80,12 +74,6 @@ def get_attention_opts(parser):
         action="store_true",
         help="Use k random distractors to compute the attention vector",
     )
-    group.add_argument(
-        "--global_context",
-        default=False,
-        action="store_true",
-        help="Use k random distractors to compute the attention vector",
-    )
 
 
 def get_game_arch_opts(parser):
@@ -99,8 +87,26 @@ def get_game_arch_opts(parser):
     group.add_argument(
         "--output_dim",
         type=int,
-        default=2048,
+        default=1024,
         help="Output dim of the non-linear projection of the distractors, used to compare with msg embedding",
+    )
+    group.add_argument(
+        "--recv_separate_embeddings",
+        default=False,
+        action="store_true",
+        help="Use separate embedding matrix to transform tgt message and ctx message when attention is used",
+    )
+    group.add_argument(
+        "--sender_separate_messages",
+        default=False,
+        action="store_true",
+        help="Use separate embedding matrix to generate a tgt message and ctx message when attention is used",
+    )
+    group.add_argument(
+        "--random_ctx_position",
+        default=False,
+        action="store_true",
+        help="Randomly shuffle target and context message when attention is used",
     )
 
 
