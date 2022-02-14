95c36e00b7e177306c97d1fac43c752dabc4b07b
robertodessi <roberto.dessi11@gmail.com>
2022-02-14 04:33:12 -0800

    fix all_accs

--- egg/zoo/referential_language/eval.py
+++ egg/zoo/referential_language/eval.py
@@ -47,6 +47,10 @@ class RandomDistractorsTestLoss(nn.Module):
     ):
         bsz, max_objs = aux_input["mask"].shape
 
+        labels = aux_input["game_labels"].view(-1)
+        all_accs = (receiver_output.argmax(dim=-1) == labels).detach().float()
+        aux_input["all_accs"] = all_accs.view(bsz, max_objs, -1)
+
         labels = torch.zeros(bsz, device=receiver_output.device)
         acc = (receiver_output[0::max_objs].argmax(dim=-1) == labels).detach().float()
         return torch.zeros(1), {"acc": acc}
--- egg/zoo/referential_language/games.py
+++ egg/zoo/referential_language/games.py
@@ -40,8 +40,11 @@ class Loss(nn.Module):
         labels = aux_input["game_labels"].view(-1)
         mask = aux_input["mask"].float().view(-1)
 
+        bsz, max_objs = aux_input["mask"].shape
+        all_accs = receiver_output.argmax(dim=-1) == labels
+        aux_input["all_accs"] = all_accs.view(bsz, max_objs, -1).detach().float()
+
         if not self.training and self.random_distractors:
-            bsz, max_objs = aux_input["mask"].shape
             acc_labels = torch.zeros(bsz, device=receiver_output.device)
             acc = (
                 (receiver_output[0::max_objs].argmax(dim=-1) == acc_labels)
@@ -49,8 +52,7 @@ class Loss(nn.Module):
                 .float()
             )
         else:
-            acc = (receiver_output.argmax(dim=-1) == labels).detach().float()
-            acc *= mask  # zeroing masked elements
+            acc = all_accs * mask  # zeroing masked elements
             acc = (acc.sum() / mask.sum()).unsqueeze(0)  # avoid dimensionless tensors
 
         loss = F.cross_entropy(receiver_output, labels, reduction="none")
