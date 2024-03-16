4a898eed4ea849c7326fc117f0f9f6b523e871d0
manugaurdl <manugaurwork@gmail.com>
2024-03-16 15:31:54 +0530

    untracked configs and job.sh

--- job.sh
+++
@@ -1,6 +0,0 @@
-
-source ~/venvs/egg/bin/activate
-python -m egg.zoo.emergent_captioner.finetuning.train cider coco
-python -m egg.zoo.emergent_captioner.finetuning.train cider mistral_4
-python -m egg.zoo.emergent_captioner.finetuning.train cider blip2mistral
-#echo "Training succesfull"
